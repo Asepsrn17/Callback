f041afd6eff0bc4d250817fd6de24a8255d56486
Asepsrn17 <asepsrn17@gmail.com>
2022-03-06 17:46:19 +0700

    callback

---
+++ main.js
@@ -0,0 +1,28 @@
+    const table = document.getElementById("data");
+    table.innerHTML = Table()
+
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(response => {
+        table.innerHTML = render(response);
+    });
+
+    function render(response) {
+        let table = "";
+        response.forEach((data) => {
+          table += `<tr>
+                      <td>${data.id}</td>
+                      <td>${data.name}</td>
+                      <td>${data.username}</td>
+                      <td>${data.email}</td>
+                      <td>${data.address.street}</td>
+                      <td>${data.company.name}</td>
+                    </tr>`;
+        });
+        return table;
+      }
+
+    function Table() {
+  return table;
+}
+
